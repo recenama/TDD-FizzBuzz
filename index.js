4eaf3578d0b6543647374eb0214188871512fbe5
PaolaCajilema <cajilemapaola@gmail.com>
2019-11-14 23:58:46 -0500

    Add test to verify if value is 5

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports = (num) => {
     if (num === 3) return 'Fizz';
+    return 'Buzz';
 };
--- index.test.js
+++ index.test.js
@@ -4,4 +4,7 @@ describe('FizzBuzz', () => {
     it('should return Fizz if value is 3', () => {
         expect(fizzbuzz(3)).toBe('Fizz')
     })
+    it('should return Buzz if value is 5', () => {
+        expect(fizzbuzz(5)).toBe('Buzz')
+    })
 })
