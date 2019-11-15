7beb540ad0bc5ad8e3ad9fb69d9088d206a36eb1
PaolaCajilema <cajilemapaola@gmail.com>
2019-11-15 00:15:30 -0500

    Refactoring

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = (num) => {
-    if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
-    if (num % 3 === 0) return 'Fizz';
-    if (num % 5 === 0) return 'Buzz';
+    const isMultipleOf3 = num % 3 === 0;
+    const isMultipleOf5 = num % 5 === 0;
+    if (isMultipleOf3 && isMultipleOf5) return 'FizzBuzz';
+    if (isMultipleOf3) return 'Fizz';
+    if (isMultipleOf5) return 'Buzz';
     return num;
 };
